5eea1173abc9de1dd196a7b73b6bd1f9535064a7
Fenton Travers <fenton.travers@gmail.com>
2016-05-26 16:01:12 -0700

    update url

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject cljs-websocket-coreasync-simple "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/ftravers/cljs-websocket-coreasync-simple"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]])
