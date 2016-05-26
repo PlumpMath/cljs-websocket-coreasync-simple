f0c140f1d859830e544244545b4d226222c08936
Fenton Travers <fenton.travers@gmail.com>
2016-05-26 15:53:26 -0700

    The initial commit.

---
+++ test/cljs_websocket_coreasync_simple/core_test.clj
@@ -0,0 +1,7 @@
+(ns cljs-websocket-coreasync-simple.core-test
+  (:require [clojure.test :refer :all]
+            [cljs-websocket-coreasync-simple.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
