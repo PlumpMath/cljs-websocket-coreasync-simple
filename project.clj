492c4c57538e8ae5d0ac1bb81f2614fda7e4113e
Fenton Travers <fenton.travers@gmail.com>
2016-05-26 17:34:22 -0700

    change to cljs file.

--- project.clj
+++ project.clj
@@ -3,4 +3,10 @@
   :url "https://github.com/ftravers/cljs-websocket-coreasync-simple"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/clojurescript "1.8.51"]
+                 [org.clojure/core.async "0.2.374"
+                  :exclusions [org.clojure/tools.reader]]
+                 ]
+
+  )
